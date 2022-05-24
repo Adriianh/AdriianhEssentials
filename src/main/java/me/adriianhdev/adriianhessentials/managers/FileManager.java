0519d1a41d961cea2a94527dbbba43d3f97804ad
Armagidon <yt.armagidon.mc@gmail.com>
2022-05-24 23:09:49 +0300

    Added Collectors import

--- src/main/java/me/adriianhdev/adriianhessentials/managers/FileManager.java
+++ src/main/java/me/adriianhdev/adriianhessentials/managers/FileManager.java
@@ -11,6 +11,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class FileManager extends YamlConfiguration {
 
